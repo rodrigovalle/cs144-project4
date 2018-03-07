c6c42c4060bf4b7b80bf5e180186ef357a555842
Rodrigo Valle <rdan.valle@gmail.com>
2018-03-06 22:02:41 -0800

    Fix robustness issues with /login

--- routes/login.js
+++ routes/login.js
@@ -12,23 +12,27 @@ const options = {
 
 router.get('/', (req, res) => {
     let {username, password, redirect} = req.query;
-    let {secret} = req.app.locals;
+    let secret = req.app.locals.secret;
 
-    if (!(username && password)) {
-        res.render('login');
+    if (!(username || password)) {
+        res.render('login', {retry: false});
+        return;
+    } else if (!(username && password)) {
+        res.render('login', {retry: true});
+        return;
     }
 
     db_manager.connect()
      .then(db => db.collection('Users'))
      .then(collection => collection.findOne({username: username}))
-     .then(user => bcrypt.compare(password, user.password))
+     .then(user => user ? bcrypt.compare(password, user.password) : Promise.resolve(false))
      .then((success) => {
         if (success) {
-            let token = jwt.sign({usr: req.query.username}, secret, options);
+            let token = jwt.sign({usr: username}, secret, options);
             res.cookie('jwt', token, {expires: 0});
             res.redirect(redirect ? redirect : '/blog/' + username);
         } else {
-            res.render('login');
+            res.render('login', {retry: true});
         }
      }).catch(err => console.log("login error: " + err.message));
 });
